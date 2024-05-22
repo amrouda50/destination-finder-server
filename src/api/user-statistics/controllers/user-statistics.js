fa61ca0a04ba1be6a48ec65a39be7c10e9131712
havenousername <andrei.cristea997@gmail.com>
2024-05-22 03:41:16 +0200

    feat: create `PUT` route for the favourites

--- src/api/user-statistics/controllers/user-statistics.js
+++ src/api/user-statistics/controllers/user-statistics.js
@@ -14,5 +14,19 @@ module.exports = createCoreController('api::user-statistics.user-statistics',  (
         }
       });
     return ctx.send(statistics.favouriteRegions);
+  },
+  async updateFavourites(ctx) {
+    const user = ctx.params['user'];
+    const favourites = ctx.request.body;
+    const statistics = await strapi.db
+      .query('api::user-statistics.user-statistics')
+      .findOne({
+        where: {user},
+      });
+
+    const updateRes = await strapi.entityService.update('api::user-statistics.user-statistics', statistics.id, {
+      data: { favouriteRegions: favourites }
+    })
+    return ctx.send(updateRes);
   }
 }));
